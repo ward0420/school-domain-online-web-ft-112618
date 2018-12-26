1ba6dafaacc55850279667c8e28d3a8a42d81768
Don Ward <10is0420@gmail.com>
2018-12-26 12:32:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,3 +1,15 @@
 class School
+  attr_accessor :name
   
+  roster = {}
+  
+  def intialized (roster)
+    @roster = roster
+  end
+  
+  def add_student(name, grade)
+    roster[:name] = name
+    roster[:grade] = grade
+    
+  end
 end
